a2c1f60b0fa3b24cd6156a73b0c83b6358147964
James Leighton <github@jamesleighton.com>
2019-01-03 21:08:26 +0000

    untappd latest checkin

--- untappd.py
+++ untappd.py
@@ -4,17 +4,29 @@ import requests
 import json
 import secrets
 
+
 url = 'https://api.untappd.com/v4/user/checkins/' + secrets.untappd_username + '?client_id=' + secrets.untappd_clientid + '&client_secret=' + secrets.untappd_secret
 untappd = requests.get(url)
 json_data = json.loads(untappd.text)
-
+#load the json structure
 LatestBeers = json_data["response"]["checkins"]
-print (LatestBeers)
 
-#LatestBeer = json_data["count"]["items"]
+#go to the items key and pick the top one
+LatestBeers = LatestBeers["items"][0]
 
-#print (LatestBeer.keys())
+print (LatestBeers.keys())
 
-#LatestBeerNames = json_data["items"]["0"]
+LatestBeer = (LatestBeers["beer"]["beer_name"])
+LatestBrewery = (LatestBeers["brewery"]["brewery_name"])
 
-#print (LatestBeerName)
+LatestRating = (LatestBeers["rating_score"])
+LatestDate = (LatestBeers["created_at"])
+LatestVenue = (LatestBeers["venue"]["venue_name"])
+LatestComment = (LatestBeers["checkin_comment"])
+LatestToasts = (LatestBeers["toasts"]["total_count"])
+
+print ("%s by %s at %s" % (LatestBeer, LatestBrewery, LatestVenue))
+print ("Rating: %s/5" % LatestRating)
+print ("Date: %s" % LatestDate)
+print ("Comment: %s" % LatestComment)
+print ("Toasted %s times" % LatestToasts)
